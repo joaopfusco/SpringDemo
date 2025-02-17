78f2df805383f60d10c6da3cce313198322b7bc4
JPFUSCO_LAPTOP\joaop <joaopedrofusco@gmail.com>
2025-02-17 12:57:41 -0300

    adicionando JsonProperty para id, createdAt e updatedAt

--- src/main/java/com/example/demo/Domain/Models/BaseModel.java
+++ src/main/java/com/example/demo/Domain/Models/BaseModel.java
@@ -1,5 +1,6 @@
 package com.example.demo.Domain.Models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,12 +17,15 @@ public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, nullable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     @CreationTimestamp
     @Column(updatable = false)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime updatedAt;
 }
